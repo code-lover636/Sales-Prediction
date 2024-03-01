8748991070bd99200be44563f26dac80d061c37c
Aravind Ashokan <aravindashokan636@gmail.com>
2024-03-01 22:28:11 +0530

    CSV file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Sales-Prediction
+
+## About
 Sales prediction with machine learning (done as a part of NeuralNet competition conducted by Excel MEC)
+
+Model used: **Random forest Regressor**
+
+
+## Installation
+```
+pip install pandas scikit-learn scikit-learn-intelex xgboost
+```
+
+
+
+
--- main.py
+++ main.py
@@ -1,48 +1,8 @@
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import mean_squared_error
 import pandas as pd
+import pickle 
 
-# Load the dataset
-data = pd.read_csv("dataset/train.csv")
-data.dropna(inplace=True)
+with open('model1.pkl', 'rb') as file:
+    model = pickle.load(file)
 
-# Split the data into features (X) and target variable (Y)
-X = data.drop('num_sold', axis=1)
-Y = data['num_sold']
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-
-# Define categorical features and preprocessing steps
-categorical_features = ['state', 'store', 'product']
-categorical_transformer = Pipeline(steps=[
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))
-])
-
-# Preprocessor for categorical features
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('cat', categorical_transformer, categorical_features)
-    ])
-
-# Define the Random Forest regressor model
-rf_regressor = Pipeline(steps=[('preprocessor', preprocessor),
-                                ('regressor', RandomForestRegressor(random_state=0))])
-
-# Train the Random Forest regressor
-rf_regressor.fit(X_train, Y_train)
-
-# Make predictions on the test data
-Y_pred = rf_regressor.predict(X_test)
-
-# Calculate Mean Squared Error
-mse = mean_squared_error(Y_test, Y_pred)
-print("Mean Squared Error:", mse)
-
-from sklearn.metrics import r2_score
-
-# Calculate R2 score
-r2 = r2_score(Y_test, Y_pred)
-print("R2 Score:", r2)
+k = model.predict(pd.read_csv())
+print(k)
